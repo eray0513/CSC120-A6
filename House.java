d66f5bc37218678aef6362134ae6faf0176c7a46
Evelyn Ray <eray@smith.edu>
2024-10-28 17:05:40 -0400

    Formatting and reflection/rubric

--- House.java
+++ House.java
@@ -1,10 +1,10 @@
 // import the ArrayList class
 import java.util.ArrayList;
 /* This is a stub for the House class */
-public class House extends Building{
+public class House extends Building {
 
   // Keeps track of the list of people who lives there
-  private ArrayList<String> residents;
+  private ArrayList < String > residents;
   // Indicates if the house has a dining room
   private boolean hasDiningRoom;
 
@@ -17,7 +17,7 @@ public class House extends Building{
    */
   public House(String name, String address, int nFloors, boolean dining) {
     super(name, address, nFloors);
-    this.residents = new ArrayList<String>();
+    this.residents = new ArrayList < String > ();
     this.hasDiningRoom = dining;
     System.out.println("You have built a house: 🏠");
   }
@@ -26,7 +26,7 @@ public class House extends Building{
    * Accessor for dining room boolean
    * @return T/F if there is a dining room
    */
-  public boolean hasDiningRoom(){
+  public boolean hasDiningRoom() {
     return this.hasDiningRoom;
   }
 
@@ -34,7 +34,7 @@ public class House extends Building{
    * Accessor for number of residents
    * @return size of residents array list
    */
-  public int nResidents(){
+  public int nResidents() {
     return this.residents.size();
   }
 
@@ -42,10 +42,10 @@ public class House extends Building{
    * Allows resident to move in if not already living there
    * @param name String object representing resident
    */
-  public void moveIn(String name){
-    if(this.isResident(name)){
+  public void moveIn(String name) {
+    if (this.isResident(name)) {
       System.out.println(name + " already lives here. Move in failed.");
-    }else{
+    } else {
       this.residents.add(name);
       System.out.println(name + " has successfully moved in.");
     }
@@ -56,11 +56,11 @@ public class House extends Building{
    * @param name String object of person moving in
    * @return the resident's name if they are moved in and a message if not
    */
-  public String moveOut(String name){
-    if(this.isResident(name)){
+  public String moveOut(String name) {
+    if (this.isResident(name)) {
       this.residents.remove(name);
       return name;
-    } else{
+    } else {
       String notAdded = name + " does not live here. Move out failed.";
       return notAdded;
     }
@@ -71,10 +71,10 @@ public class House extends Building{
    * @param name resident to be checked for
    * @return T/F if the person is a resident
    */
-  public boolean isResident(String name){
-    if(this.residents.contains(name)){
+  public boolean isResident(String name) {
+    if (this.residents.contains(name)) {
       return true;
-    } else{
+    } else {
       return false;
     }
   }
--- Library.java
+++ Library.java
@@ -1,148 +1,145 @@
 // import Hashtable class
 import java.util.Hashtable;
 /* This is a stub for the Library class */
-public class Library extends Building{
+public class Library extends Building {
 
-    // Creates a hashtable to store the books and whether they are available
-    private Hashtable<String, Boolean> collection;
+  // Creates a hashtable to store the books and whether they are available
+  private Hashtable < String, Boolean > collection;
 
-    /**
-     * Constructs a library object that uses the building superclass and initializes the collection to be empty
-     * @param name name of library (superclass)
-     * @param address address of library (superclass)
-     * @param nFloors number of floors (superclass)
-     */
-    public Library(String name, String address, int nFloors) {
-      super(name, address, nFloors);
-      this.collection = new Hashtable<String, Boolean>();
-      System.out.println("You have built a library: 📖");
-    }
+  /**
+   * Constructs a library object that uses the building superclass and initializes the collection to be empty
+   * @param name name of library (superclass)
+   * @param address address of library (superclass)
+   * @param nFloors number of floors (superclass)
+   */
+  public Library(String name, String address, int nFloors) {
+    super(name, address, nFloors);
+    this.collection = new Hashtable < String, Boolean > ();
+    System.out.println("You have built a library: 📖");
+  }
 
-    /**
-     * Add a title to the collection and set it to available (true)
-     * @param title of book
-     */
-    public void addTitle(String title){
-      this.collection.put(title, true);
-    }
+  /**
+   * Add a title to the collection and set it to available (true)
+   * @param title of book
+   */
+  public void addTitle(String title) {
+    this.collection.put(title, true);
+  }
 
-    /**
-     * Removes a title from the collection
-     * @param title of the book (key)
-     * @return String of book that was removed or error message
-     */
-    public String removeTitle(String title){
-      String result;
-      if(this.containsTitle(title)){
-        this.collection.remove(title, true);
-        result = title;
-        return result;
-      } else{
-        result = "The library does not contain this title";
-        return result;
-      }
-    }
-
-    /**
-     * Allows books to be checked out if they are in the library and available
-     * @param title of book being checked out (key)
-     */
-    public void checkOut(String title){
-      if(this.containsTitle(title)){
-        if(this.isAvailable(title)){
-        this.collection.replace(title, false);
-        } else{
-          System.out.println("This title is not available at this time.");
-        }
-      } else{
-        System.out.println("This library does not contain this title");
-      }
-    }
-
-    /**
-     * Allows books to be returned to the library
-     * @param title of the book (key)
-     */
-    public void returnBook(String title){
-      if(this.containsTitle(title)){
-        this.collection.replace(title, true);
-      } else{
-        System.out.println("The library does not contain this title.");
-      }
-    }
-
-    /**
-     * Checks if the collection contains a title
-     * @param title of the book (key)
-     * @return T/F if the title is in the collection
-     */
-    public boolean containsTitle(String title){
-      if(this.collection.containsKey(title)){
-        return true;
-      } else{
-        return false;
-      }
-    }
-
-    /**
-     * Checks if a title is available
-     * @param title of the book (key)
-     * @return T/F if the book is available
-     */
-    public boolean isAvailable(String title){
-      boolean result = false;
-      if(this.collection.get(title)==true){
-        result = true;
-      }
+  /**
+   * Removes a title from the collection
+   * @param title of the book (key)
+   * @return String of book that was removed or error message
+   */
+  public String removeTitle(String title) {
+    String result;
+    if (this.containsTitle(title)) {
+      this.collection.remove(title, true);
+      result = title;
+      return result;
+    } else {
+      result = "The library does not contain this title";
       return result;
     }
-
-    /**
-     * Prints the collection by iterating over the hashtable
-     */
-    public void printCollection(){
-      System.out.println("\nCollection: " +"\n");
-      for(String key : this.collection.keySet()){
-        String available = "Unavailable";
-        if(this.collection.get(key) == true){
-          available = "Available";
-        }
-        System.out.println(key + ": " + available);
-      }
-    }
-
-  
-    public static void main(String[] args) {
-        // Create a library object
-        Library myLibrary = new Library("Neilson", "1 Green St", 4);
-        // Create book objects
-        String book = "Percy Jackson by Rick Riordan";
-        String book2 = "The Mysterious Benedict Society by Lisa Lisa";
-        String book3 = "I Can't Think of Anything by Evelyn Ray";
-        String book4 = "Free Will by Rvelyn Eay";
-
-        // Test add books to collection
-        myLibrary.addTitle(book);
-        myLibrary.addTitle(book2);
-        myLibrary.addTitle(book3);
-        myLibrary.addTitle(book4);
-
-        // Test removing books
-        System.out.println(myLibrary.removeTitle(book));
-        System.out.println(myLibrary.removeTitle(book));
-        myLibrary.addTitle(book);
-
-        // Test checking out and returning books
-        myLibrary.checkOut(book);
-        myLibrary.checkOut(book);
-        myLibrary.returnBook(book);
-        myLibrary.checkOut(book);
-
-        // Test printing collection
-        myLibrary.printCollection();
-
-
-
-    }
-  
   }
+
+  /**
+   * Allows books to be checked out if they are in the library and available
+   * @param title of book being checked out (key)
+   */
+  public void checkOut(String title) {
+    if (this.containsTitle(title)) {
+      if (this.isAvailable(title)) {
+        this.collection.replace(title, false);
+      } else {
+        System.out.println("This title is not available at this time.");
+      }
+    } else {
+      System.out.println("This library does not contain this title");
+    }
+  }
+
+  /**
+   * Allows books to be returned to the library
+   * @param title of the book (key)
+   */
+  public void returnBook(String title) {
+    if (this.containsTitle(title)) {
+      this.collection.replace(title, true);
+    } else {
+      System.out.println("The library does not contain this title.");
+    }
+  }
+
+  /**
+   * Checks if the collection contains a title
+   * @param title of the book (key)
+   * @return T/F if the title is in the collection
+   */
+  public boolean containsTitle(String title) {
+    if (this.collection.containsKey(title)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Checks if a title is available
+   * @param title of the book (key)
+   * @return T/F if the book is available
+   */
+  public boolean isAvailable(String title) {
+    boolean result = false;
+    if (this.collection.get(title) == true) {
+      result = true;
+    }
+    return result;
+  }
+
+  /**
+   * Prints the collection by iterating over the hashtable
+   */
+  public void printCollection() {
+    System.out.println("\nCollection: " + "\n");
+    for (String key: this.collection.keySet()) {
+      String available = "Unavailable";
+      if (this.collection.get(key) == true) {
+        available = "Available";
+      }
+      System.out.println(key + ": " + available);
+    }
+  }
+
+  public static void main(String[] args) {
+    // Create a library object
+    Library myLibrary = new Library("Neilson", "1 Green St", 4);
+    // Create book objects
+    String book = "Percy Jackson by Rick Riordan";
+    String book2 = "The Mysterious Benedict Society by Lisa Lisa";
+    String book3 = "I Can't Think of Anything by Evelyn Ray";
+    String book4 = "Free Will by Rvelyn Eay";
+
+    // Test add books to collection
+    myLibrary.addTitle(book);
+    myLibrary.addTitle(book2);
+    myLibrary.addTitle(book3);
+    myLibrary.addTitle(book4);
+
+    // Test removing books
+    System.out.println(myLibrary.removeTitle(book));
+    System.out.println(myLibrary.removeTitle(book));
+    myLibrary.addTitle(book);
+
+    // Test checking out and returning books
+    myLibrary.checkOut(book);
+    myLibrary.checkOut(book);
+    myLibrary.returnBook(book);
+    myLibrary.checkOut(book);
+
+    // Test printing collection
+    myLibrary.printCollection();
+
+  }
+
+}
